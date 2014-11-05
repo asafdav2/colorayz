301df2732a902b3f50d4bb1e875176e1c2b279cf
asafdav2 <asafdav2@gmail.com>
2014-11-05 19:05:27 +0200

    add undo/redo functionality

--- client/app/colorize/colorize.controller.js
+++ client/app/colorize/colorize.controller.js
@@ -130,6 +130,7 @@ angular.module('colorayzApp')
                         isDrawing = true;
                         updateLocation(e);
                         ctx.beginPath();
+                        history.saveState(this);
                         ctx.fillStyle = $scope.brushColor;
                         ctx.lineJoin = ctx.lineCap = 'round';
                         ctx.lineWidth = $scope.brushWidth;
@@ -323,4 +324,51 @@ angular.module('colorayzApp')
             $scope.srcCanvas.style.cursor = 'url(' + cursor.toDataURL() + ') ' + cursorHalfSize + ' ' + cursorHalfSize + ', auto';
         }
 
+        var history = {
+            redo_list: [],
+            undo_list: [],
+            saveState: function(canvas, list, keep_redo) {
+                keep_redo = keep_redo || false;
+                if(!keep_redo) {
+                    this.redo_list = [];
+                }
+
+                (list || this.undo_list).push(canvas.toDataURL());
+            },
+            undo: function(canvas, ctx) {
+                this.restoreState(canvas, ctx, this.undo_list, this.redo_list);
+            },
+            redo: function(canvas, ctx) {
+                this.restoreState(canvas, ctx, this.redo_list, this.undo_list);
+            },
+            restoreState: function(canvas, ctx,  pop, push) {
+                if(pop.length) {
+                    this.saveState(canvas, push, true);
+                    var restore_state = pop.pop();
+                    var img = new Image();
+                    img.src = restore_state;
+
+                    img.onload = function() {
+                        ctx.clearRect(0, 0, $scope.width, $scope.height);
+                        ctx.drawImage(img, 0, 0);
+                    }
+                }
+            }
+        };
+
+        $scope.undo = function() {
+            history.undo($scope.srcCanvas,  $scope.srcCanvas.getContext('2d'));
+        };
+
+        $scope.redo = function() {
+            history.redo($scope.srcCanvas,  $scope.srcCanvas.getContext('2d'));
+        };
+
+        document.onkeydown = KeyPress;
+
+        function KeyPress(e) {
+            var evtobj = window.event? event : e;
+            if (evtobj.keyCode == 90 && evtobj.ctrlKey) $scope.undo();
+            if (evtobj.keyCode == 89 && evtobj.ctrlKey) $scope.redo();
+        }
   }]);
